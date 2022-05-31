02421bb4e2942f46fc2f1ec772937bf33bb978f3
andrea <andrealescano94@gmail.com>
2022-05-31 09:59:35 -0300

    mod new JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -59,8 +59,6 @@ function agregarStringInvertida() {
   class Persona {
     constructor(nombre,apellido,edad,domicilio) {
       // Crea el constructor:
-
-     
         this.nombre=nombre;
         this.apellido=apellido;
         this.edad=edad;
